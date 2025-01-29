a2867c942841610c0a66dd378032f8e469f5cf46
Kimberly <149002269+kimbebebe@users.noreply.github.com>
2025-01-29 16:55:06 +0000

    Updated thoughts() and added past project
    Updated Thoughts() and added a new thought to the list
    Added Danganronpa as a past project in Projects page

--- code.js
+++ code.js
@@ -9,18 +9,19 @@ function PickThought(thoughts)
             "Make sure you look after yourself today!",
             "Tell someone you love them!",
             "Why does the alphabet start with A?",
-            "I like the Norfolk beaches.",
+            "Have you visited the beaches in North Norfolk?",
             "It's 9000 miles back to you",
             "Hello there! :)",
             "If it's the worst day of your life, then tomorrow can only be better",
             "Refresh the page! These change!",
-            "I like storms and planes",
+            "Storm Éowyn (2024) reached windspeeds of 114mph in County Gallway, Ireland... Then the recording equipment broke!",
             "I like challenging myself!",
             "Welcome, traveller!",
             "Why do Americans call them 'English muffins'?",
             "You found me!",
             "I'm not a graphics designer, but I tried!",
             "Do you like chocolate pizza?",
-            "Welcome to my personal website!"]
+            "Welcome to my personal website!",
+            "I'm hungry..."]
 
             document.getElementById("picker").innerHTML = PickThought(thoughts);
